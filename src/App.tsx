148aa9730ccafc0d310f55036ba9d70271a8e111
JoaoVSouto <joaovitorss99@hotmail.com>
2020-06-28 18:02:32 -0300

    Styles server list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import Layout from './components/Layout';
+import ServerList from './components/ServerList';
+
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
     <>
-      <Layout />
+      <Layout>
+        <ServerList />
+      </Layout>
 
       <GlobalStyles />
     </>
