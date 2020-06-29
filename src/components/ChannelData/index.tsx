36687527f59f411e14be110c6a05750dd28b4db7
JoaoVSouto <joaovitorss99@hotmail.com>
2020-06-29 13:02:57 -0300

    Styles ChannelData's messages

--- src/components/ChannelData/index.tsx
+++ src/components/ChannelData/index.tsx
@@ -2,10 +2,38 @@ import React from 'react';
 
 import { Container, Messages, InputWrapper, Input, InputIcon } from './styles';
 
+import ChannelMessage, { Mention } from '../ChannelMessage';
+
 const ChannelData: React.FC = () => {
   return (
     <Container>
-      <Messages />
+      <Messages>
+        {Array(15)
+          .fill(0)
+          .map((_, i) => (
+            <ChannelMessage
+              key={i}
+              author="João Vítor"
+              date="29/06/2020"
+              avatar="https://api.adorable.io/avatars/face/eyes5/nose7/mouth9/6633cc"
+              content="Recriando a interface do Discord guys!"
+            />
+          ))}
+
+        <ChannelMessage
+          author="Pedro Lucas"
+          date="29/06/2020"
+          avatar="https://api.adorable.io/avatars/face/eyes2/nose4/mouth1/008eff"
+          content={
+            <>
+              <Mention>@João Vítor</Mention>, me empresta o controle do
+              Playstation pfv??
+            </>
+          }
+          hasMention
+          isBot
+        />
+      </Messages>
 
       <InputWrapper>
         <Input placeholder="Conversar em #chat-livre" />
